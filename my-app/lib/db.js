ed2f75ff438b310a765f3a588a47958cfd367616
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-10-03 02:19:43 +0200

    adding more packages

--- my-app/lib/db.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    'mongodb+srv://lentsweletlotlo0:chrismokotedi@cluster0.iskpsdl.mongodb.net/events?retryWrites=true&w=majority&appName=AtlasApp'
-  );
-
-  return client;
-}
