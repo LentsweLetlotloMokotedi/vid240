e656abbb6b98e072e3ac832b974d52b37cdfd037
Lentswe Letlotlo Mokotedi <lentsweletlotlo0@gmail.com>
2024-04-02 17:55:02 +0200

    adding more style

--- my-app/components/starting-page/starting-page.js
+++ my-app/components/starting-page/starting-page.js
@@ -1,11 +1,22 @@
 import classes from './starting-page.module.css';
+import { useState, useEffect } from 'react';
 
 function StartingPageContent() {
-  // Show Link to Login page if NOT auth
+  const [animateLetters, setAnimateLetters] = useState(false);
+
+  // Add animation effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimateLetters(prevState => !prevState);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <section className={classes.starting}>
-      <span><h1>Welcome on Board!</h1></span>
+      <span className={animateLetters ? classes.animated : ''}>
+        <h1>Welcome on Board!</h1>
+      </span>
     </section>
   );
 }
